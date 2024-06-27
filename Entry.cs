48b3d538aec8ab5820d4edd1c8f293ce280c3598
Muath Mhawich <23041194@student.ltuc.com>
2024-06-27 08:55:29 +0300

    "create console app  (daily diary)  lab-5 "

---
+++ Entry.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Daily_Diary
+{
+    public class Entry
+    {
+        public DateTime Date { get; set; }
+        public string Content { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Date:s}: {Content}";
+        }
+    
+}
+}
